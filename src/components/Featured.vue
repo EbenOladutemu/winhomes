4fbf08b96c84c528b4b17f3c9817d6b7c7bbe33f
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2020-08-06 17:24:31 +0100

    Update Featured

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -9,7 +9,40 @@
             <span class="text-dark">Winhome Estates</span>
           </h4>
             <div class="img_blog">
-              <img src="../assets/img/projects/3-bedroom.jpg" alt="" class="img-fluid rounded mx-auto d-block">
+              <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-interval="10000">
+                    <!-- <img src="..." class="d-block w-100" alt="..."> -->
+                    <img src="../assets/img/projects/3-bedroom.jpg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item" data-interval="2000">
+                    <img src="../assets/img/projects/winhomes-2.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="../assets/img/projects/winhomes-3.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="../assets/img/projects/winhomes-4.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="../assets/img/projects/winhomes-5.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="../assets/img/projects/winhomes-6.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="../assets/img/projects/winhomes-7.jpeg" alt="" class="img-fluid rounded mx-auto d-block">
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
             </div>
             <div class="content_blog">
               <div class="mt-3">
@@ -17,16 +50,16 @@
                 <p class="mt-3 desc_blog">
                   We are currently selling plots of land in Winhomes estate located at Okun Ajah town in Eti-Osa LGA, Lagos state.<br> This fast selling property has the following amazing features:
                 </p>
-                <ul class="pl-2 mt-2">
-                  <li>❖	Title: GOVERNOR’S CONSENT</li>
-                  <li>❖	Dry Land (available and ready for immediate construction) </li>
-                  <li>❖	Survey plan for the plot spaces</li>
-                  <li>❖	No hidden charges (Development fee inclusive, Planned infrastructures and amenities which includes: power supply, paved roads, covered drainages, water supply, perimeter fencing, street lights, etc.) </li>
-                  <li>❖	Commercial area</li>
-                  <li>❖	Restricted access</li>
-                  <li>❖	School</li>
-                  <li>❖	Recreation facilities (water park)</li>
-                </ul>
+                <h5 class="text-bold">Title: GOVERNOR’S CONSENT</h5>
+                <ol class="pl-2 mt-2">
+                  <li>Dry Land (available and ready for immediate construction) </li>
+                  <li>Survey plan for the plot spaces</li>
+                  <li>No hidden charges (Development fee inclusive, Planned infrastructures and amenities which includes: power supply, paved roads, covered drainages, water supply, perimeter fencing, street lights, etc.) </li>
+                  <li>Commercial area</li>
+                  <li>Restricted access</li>
+                  <li>School</li>
+                  <li>Recreation facilities (water park)</li>
+                </ol>
                 <div v-show="show">
                   <p>
                     <strong>Great location:</strong> Proximity to Chevron, Lekki conservation, Lekki Epe express via Orchid Hotel Road, proposed coastal road running from Victoria Island/Ikoyi to Ibeju Lekki and several other prime spots (Prime mall, Lekki Miami beach), access through Ogombo road, located directly behind coop Lag Estate.
@@ -35,13 +68,13 @@
                   <p>
                     Our offer is at the most competitive prices and also includes flexible payment plans for interested buyers One plot of land (600/sqm) is ₦20,000,000 (₦33.333/sqm). Other available plot sizes includes: 300sqm, 500sqm and 750sqm.
                   </p>
-                  <ul class="pl-2 mt-2">
-                    <li>✓	This price includes development fee as drainage works and paved road construction works are currently on going</li>
-                    <li>✓	Available flexible payments plans include: 30% down payment (minimum) and the balance spreads evenly over 3, 6 or 12 months as it’s convenient for the buyer</li>
-                    <li>✓	No hidden charges</li>
-                    <li>✓	Allocation confirmed upon receipt of the 30% down payment and a buyer specific sales agreement issued to the buyer stating the terms of the contract pending the final liquidation of the outstanding amount.</li>
-                    <li>✓	We also provide an inbound sales and after-sales service to every buyer helping you make the best buying decisions and also ensure that your after-purchase experience is euphoric.</li>
-                  </ul>
+                  <ol class="pl-2 mt-2">
+                    <li>This price includes development fee as drainage works and paved road construction works are currently on going</li>
+                    <li>Available flexible payments plans include: 30% down payment (minimum) and the balance spreads evenly over 3, 6 or 12 months as it’s convenient for the buyer</li>
+                    <li>No hidden charges</li>
+                    <li>Allocation confirmed upon receipt of the 30% down payment and a buyer specific sales agreement issued to the buyer stating the terms of the contract pending the final liquidation of the outstanding amount.</li>
+                    <li>We also provide an inbound sales and after-sales service to every buyer helping you make the best buying decisions and also ensure that your after-purchase experience is euphoric.</li>
+                  </ol>
                 </div>
                 <button class="btn btn-custom" @click="readMore">
                   <span v-if="!show">Read More</span>
@@ -73,7 +106,8 @@ export default {
 
 <style scoped>
 li{
-  list-style: none;
+  list-style: inside;
+  list-style-type: disc;
 }
 .btn-custom{
   padding: 5px 4px;
